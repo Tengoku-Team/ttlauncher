491d52c3aad8980535d44891813d91e13d8da592
MYZT3RY <d1maz.d@yandex.ru>
2021-02-10 21:03:28 +0300

    added sampqueryapi clas

--- TTLauncher/Form1.cs
+++ TTLauncher/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using SampQueryApi;
 
 namespace TTLauncher{
     public partial class Form1 : Form{

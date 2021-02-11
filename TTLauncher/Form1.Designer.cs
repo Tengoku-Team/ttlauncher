81b6705d0e9872c067d003c4c2133880ffbc9e90
MYZT3RY <d1maz.d@yandex.ru>
2021-02-11 15:03:43 +0300

    reworked design

--- TTLauncher/Form1.Designer.cs
+++ TTLauncher/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.playButton = new System.Windows.Forms.Button();
             this.nicknameLabel = new System.Windows.Forms.Label();
             this.nicknameTextBox = new System.Windows.Forms.TextBox();
+            this.settingsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // playButton
             // 
-            this.playButton.Location = new System.Drawing.Point(267, 12);
+            this.playButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.playButton.Location = new System.Drawing.Point(12, 52);
             this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(96, 23);
+            this.playButton.Size = new System.Drawing.Size(103, 31);
             this.playButton.TabIndex = 0;
             this.playButton.Text = "Подключиться";
             this.playButton.UseVisualStyleBackColor = true;
@@ -46,26 +48,39 @@
             // nicknameLabel
             // 
             this.nicknameLabel.AutoSize = true;
-            this.nicknameLabel.Location = new System.Drawing.Point(12, 17);
+            this.nicknameLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nicknameLabel.Location = new System.Drawing.Point(9, 9);
             this.nicknameLabel.Name = "nicknameLabel";
-            this.nicknameLabel.Size = new System.Drawing.Size(53, 13);
+            this.nicknameLabel.Size = new System.Drawing.Size(57, 15);
             this.nicknameLabel.TabIndex = 2;
             this.nicknameLabel.Text = "Никнейм";
             // 
             // nicknameTextBox
             // 
-            this.nicknameTextBox.Location = new System.Drawing.Point(71, 14);
+            this.nicknameTextBox.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nicknameTextBox.Location = new System.Drawing.Point(12, 25);
             this.nicknameTextBox.MaxLength = 24;
             this.nicknameTextBox.Name = "nicknameTextBox";
-            this.nicknameTextBox.Size = new System.Drawing.Size(190, 20);
+            this.nicknameTextBox.Size = new System.Drawing.Size(197, 21);
             this.nicknameTextBox.TabIndex = 3;
-            this.nicknameTextBox.Text = "werwrer";
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.BackgroundImage = global::TTLauncher.Properties.Resources.gear;
+            this.settingsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.settingsButton.Location = new System.Drawing.Point(179, 52);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(30, 31);
+            this.settingsButton.TabIndex = 4;
+            this.settingsButton.UseVisualStyleBackColor = true;
+            this.settingsButton.Click += new System.EventHandler(this.settingsButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 51);
+            this.ClientSize = new System.Drawing.Size(221, 89);
+            this.Controls.Add(this.settingsButton);
             this.Controls.Add(this.nicknameLabel);
             this.Controls.Add(this.nicknameTextBox);
             this.Controls.Add(this.playButton);
@@ -83,6 +98,7 @@
         private System.Windows.Forms.Button playButton;
         private System.Windows.Forms.Label nicknameLabel;
         private System.Windows.Forms.TextBox nicknameTextBox;
+        private System.Windows.Forms.Button settingsButton;
     }
 }
 

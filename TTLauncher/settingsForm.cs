78b8a6eb0b3121a07b5532adda1ef12f3d233847
MYZT3RY <d1maz.d@yandex.ru>
2021-02-11 16:00:20 +0300

    added selecting pathofgame

--- TTLauncher/settingsForm.cs
+++ TTLauncher/settingsForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,15 @@ namespace TTLauncher{
         }
 
         private void pathToGameButton_Click(object sender, EventArgs e){
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "SAMP|samp.exe";
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK){
+                pathToGameTextBox.Text = openFileDialog.FileName;
+                RegKeys rg = new RegKeys();
+                rg.updateRegistryKey("Path", openFileDialog.FileName);
+            }
         }
     }
 }

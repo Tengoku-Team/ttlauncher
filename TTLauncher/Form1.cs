ea8a6e8bcf097b935a84cba9439f61682aa8bfba
MYZT3RY <d1maz.d@yandex.ru>
2021-02-16 01:54:41 +0300

    added info about server in listview & adaptive change width of listview and form

--- TTLauncher/Form1.cs
+++ TTLauncher/Form1.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Diagnostics;
+using SampQueryApi;
+using System.Collections.Generic;
 
 namespace TTLauncher{
     public partial class Form1 : Form{
-        private string serverIp = "127.0.0.1";//ip адрес сервера
-        private string serverPort = "7777";//порт сервера
+        //private string serverIp = "185.169.134.83";//ip адрес сервера
+        private string serverIp = "46.174.52.246";//ip адрес сервера
+        private ushort serverPort = 7777;//порт сервера
+
+        List<string> serverInfo = new List<string>();
 
         public Form1(){
             InitializeComponent();
+            SampQuery api = new SampQuery(serverIp,serverPort, 'i');
+            foreach (KeyValuePair<string, string> kvp in api.read(true)){
+                serverInfo.Add(kvp.Value);
+            }
+            if ((serverInfoListView.Width + serverInfo[3].Length) > Width){
+                Width += serverInfo[3].Length;
+                serverInfoListView.Width += serverInfo[3].Length;
+            }
+            serverInfoListView.Items.Add("Название: " + serverInfo[3]);
+            serverInfoListView.Items.Add("Название мода: " + serverInfo[4]);
+            serverInfoListView.Items.Add("Игроки: " + serverInfo[1] + "/" + serverInfo[2]);
             RegKeys rg = new RegKeys();
             if (rg.existRegistryKey()){
                 nicknameTextBox.Text = rg.getRegistryKey("Nickname");

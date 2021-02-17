c41c722fb2b22b60c16b29d2fe283cecdc67f436
MYZT3RY <d1maz.d@yandex.ru>
2021-02-17 20:28:20 +0300

    added vk & web icons

--- TTLauncher/Properties/Resources.Designer.cs
+++ TTLauncher/Properties/Resources.Designer.cs
@@ -69,5 +69,25 @@ namespace TTLauncher.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap vk {
+            get {
+                object obj = ResourceManager.GetObject("vk", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap web {
+            get {
+                object obj = ResourceManager.GetObject("web", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
